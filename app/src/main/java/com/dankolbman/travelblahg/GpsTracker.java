ae8bb7db85994d404ec13d30e075bcdad31d2873
Dan Kolbman <dankolbman@gmail.com>
2015-05-30 10:36:03 -0400

    GPS logging to file

---
+++ app/src/main/java/com/dankolbman/travelblahg/GpsTracker.java
@@ -0,0 +1,228 @@
+package com.dankolbman.travelblahg;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+/*
+GPS tracking service
+ */
+public class GpsTracker extends Service implements LocationListener {
+
+    private final Context mContext;
+    protected LocationManager locmgr;
+
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean canGetLocation = false;
+
+    Location location = null; // location
+    double latitude; // latitude
+    double longitude; // longitude
+
+    private static final long UPDATE_DIST = 10; // meters
+    private static final long UPDATE_FREQ = 1000; // milliseconds
+
+    String path = "";
+    File datafile = null;
+
+    public GpsTracker(Context context, String filename) {
+        this.mContext = context;
+        this.path = context.getFilesDir().getAbsolutePath();
+        this.path += "/" + filename;
+        this.datafile = new File(this.path);
+    }
+
+    /*
+    Start gps recording
+     */
+    public Location startRec() {
+        try {
+            locmgr = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            // update status
+            isGPSEnabled = locmgr.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locmgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                CharSequence msg = "Position not available!!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(mContext, msg, duration);
+                toast.show();
+                this.canGetLocation = false;
+            } else {
+                this.canGetLocation = true;
+
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locmgr.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                UPDATE_FREQ, UPDATE_DIST, this);
+                        CharSequence msg = "Using GPS";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(mContext, msg, duration);
+                        toast.show();
+                        if (locmgr != null) {
+                            location = locmgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                            writeLocation(location);
+                        }
+                    }
+                } else if (isNetworkEnabled) { // Prefer satellite to towers
+                    locmgr.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            UPDATE_FREQ, UPDATE_DIST, this);
+                    CharSequence msg = "Using Network";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(mContext, msg, duration);
+                    toast.show();
+                    if (locmgr != null) {
+                        location = locmgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                        writeLocation(location);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    public void stopRec(){
+        if(locmgr != null){
+            locmgr.removeUpdates(GpsTracker.this);
+            CharSequence msg = "Stopping GPS";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(mContext, msg, duration);
+            toast.show();
+            locmgr = null;
+            location = null;
+        }
+    }
+
+    /*
+    Getters
+     */
+
+    public Location getLocation() {
+        return this.location;
+    }
+    public double getLatitude(){
+        return this.latitude;
+    }
+
+    public double getLongitude(){
+       return this.longitude;
+    }
+
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    /**
+     * Function to show settings alert dialog
+     * On pressing Settings button will lauch Settings Options
+     * */
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        CharSequence msg = "Location changed!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(mContext, msg, duration);
+        toast.show();
+        this.location = location;
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
+        writeLocation(location);
+    }
+
+    /*
+    Writes location to datafile
+     */
+    public void writeLocation(Location location) {
+        try {
+
+            FileOutputStream filewriter = new FileOutputStream(this.datafile, true);
+            String out = String.format("%d, %f, %f, %f\n",
+                    location.getTime(),
+                    location.getAccuracy(),
+                    location.getLatitude(),
+                    location.getLongitude());
+
+            filewriter.write(out.getBytes());
+            filewriter.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+}
--- app/src/main/java/com/dankolbman/travelblahg/MainActivity.java
+++ app/src/main/java/com/dankolbman/travelblahg/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dankolbman.travelblahg;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,40 +10,57 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Locale;
 
 public class MainActivity extends ActionBarActivity {
+
+    private GpsTracker tracker;
 
     private EditText urlEditText;
     private EditText apiEditText;
     private String endpoint_string;
     private String api_string;
     private TextView statusTextView;
+    private TextView latitudeText;
+    private TextView longitudeText;
+    private TextView filenameText;
+
+    private TextView dataText;
+
+    private String filepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Base on the current time
+        String filename = System.currentTimeMillis()+".dat";
+        filepath = this.getFilesDir().getAbsolutePath()+filename;
+        // Set up tracker
+        tracker = new GpsTracker(this, filename);
         // Get elements
         urlEditText = (EditText) findViewById(R.id.url_endpoint);
         apiEditText = (EditText) findViewById(R.id.api_key);
         statusTextView = (TextView) findViewById(R.id.status_text);
+        latitudeText = (TextView) findViewById(R.id.latitude);
+        longitudeText = (TextView) findViewById(R.id.longitude);
+        filenameText = (TextView) findViewById(R.id.filename);
+        dataText = (TextView) findViewById(R.id.data);
+
+        filenameText.setText("Saving to: "+filename);
     }
 
     @Override
@@ -66,18 +85,67 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+    Pings server with user and auth
+     */
     public void testAPI(View view) {
         endpoint_string = urlEditText.getText().toString();
         api_string = apiEditText.getText().toString();
-        String url_string = "http://" + endpoint_string + "/api/dan/" + api_string + "/ping";
-
-        statusTextView.setText(url_string);
+        String url_string = "http://" + endpoint_string + "/" + api_string;
 
         // Begin the upload
         AsyncTaskRunner runner = new AsyncTaskRunner();
         runner.execute(url_string);
     }
 
+    /*
+    Get current location from tracker
+     */
+    public void getLoc(View view) {
+        if(tracker.canGetLocation() && tracker.getLocation() != null) {
+            Location loc = tracker.getLocation();
+            latitudeText.setText("Latitude: "+Double.toString(loc.getLatitude()));
+            longitudeText.setText("Longitude: "+Double.toString(loc.getLongitude()));
+        } else {
+            CharSequence msg = "Must be recording";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, msg, duration);
+            toast.show();
+        }
+    }
+
+    /*
+    Start gps service
+     */
+    public void startRec(View view) {
+        tracker.startRec();
+    }
+
+    /*
+    Stop gps service
+     */
+    public void stopRec(View view) {
+        tracker.stopRec();
+    }
+
+    public void getData(View view) {
+        File file = new File(filepath);
+        int length = (int) file.length();
+        byte[] bytes = new byte[length];
+        try {
+            FileInputStream in = new FileInputStream(file);
+            try {
+                in.read(bytes);
+            } finally {
+                in.close();
+            }
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        }
+
+        String contents = new String(bytes);
+        dataText.setText(contents);
+    }
     /*
     Task runner for network threads
      */
@@ -97,9 +165,10 @@ public class MainActivity extends ActionBarActivity {
                 jsonobj.put("description", "Real");
                 jsonobj.put("enable", "true");
             } catch (JSONException ex) {
-                statusTextView.setText("Error Occurred while building JSON");
+                resp = "Error Occurred while building JSON";
                 ex.printStackTrace();
             }
+
             // Connect to the api
             try {
                 URL url = new URL(params[0]);
@@ -111,6 +180,7 @@ public class MainActivity extends ActionBarActivity {
                 conn.setDoOutput(true);
                 conn.setRequestProperty("Content-Type", "application/json");
                 conn.connect();
+
                 OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(),"UTF-8");
 
                 out.write(jsonobj.toString());
