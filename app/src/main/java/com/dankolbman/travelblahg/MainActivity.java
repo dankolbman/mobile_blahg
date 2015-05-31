0beeedb84118859d27afd61cbd23181b8ef80c83
Dan Kolbman <dankolbman@gmail.com>
2015-05-30 22:56:55 -0400

    Ping uploading and history

--- app/src/main/java/com/dankolbman/travelblahg/MainActivity.java
+++ app/src/main/java/com/dankolbman/travelblahg/MainActivity.java
@@ -1,29 +1,40 @@
 package com.dankolbman.travelblahg;
 
+import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -37,10 +48,12 @@ public class MainActivity extends ActionBarActivity {
     private TextView latitudeText;
     private TextView longitudeText;
     private TextView filenameText;
+    private ProgressBar progressBarBar;
 
-    private TextView dataText;
+    private ListView fileListView;
 
-    private String filepath;
+    private File[] files;
+    private Boolean isUploading = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +61,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         // Base on the current time
         String filename = System.currentTimeMillis()+".dat";
-        filepath = this.getFilesDir().getAbsolutePath()+filename;
         // Set up tracker
         tracker = new GpsTracker(this, filename);
         // Get elements
@@ -58,9 +70,14 @@ public class MainActivity extends ActionBarActivity {
         latitudeText = (TextView) findViewById(R.id.latitude);
         longitudeText = (TextView) findViewById(R.id.longitude);
         filenameText = (TextView) findViewById(R.id.filename);
-        dataText = (TextView) findViewById(R.id.data);
+        fileListView = (ListView) findViewById(R.id.file_list);
+        progressBarBar = (ProgressBar) findViewById(R.id.progressBar);
+        this.getFilesDir().mkdirs();
+        filenameText.setText("Saving to: " + this.getFilesDir().getAbsolutePath() + "/" + filename);
 
-        filenameText.setText("Saving to: "+filename);
+        File f = new File(this.getFilesDir().getAbsolutePath());
+        files = f.listFiles();
+        statusTextView.setText(files[1].getAbsolutePath());
     }
 
     @Override
@@ -128,24 +145,211 @@ public class MainActivity extends ActionBarActivity {
         tracker.stopRec();
     }
 
-    public void getData(View view) {
-        File file = new File(filepath);
-        int length = (int) file.length();
-        byte[] bytes = new byte[length];
-        try {
-            FileInputStream in = new FileInputStream(file);
-            try {
-                in.read(bytes);
-            } finally {
-                in.close();
+    /*
+    Get files in data directory and display in list view
+     */
+    public void getFiles(View view) {
+
+        File f = new File(this.getFilesDir().getAbsolutePath());
+        files = f.listFiles();
+
+        // Add filenames to list view
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = files.length-1; i >= 0; --i) {
+            String filename = files[i].getName();
+            if(filename.substring(filename.lastIndexOf('.'),filename.length()).equals(".dat")) {
+                // Remove file extension
+                filename = filename.substring(0, filename.lastIndexOf("."));
+                // Get time in UTC from the file name
+                Date date = new Date(Long.parseLong(filename));
+                SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm:ss z");
+                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+                int numlines = 0;
+                try {
+                    numlines = countLines(files[i]);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                list.add(sdf.format(date) + " - " + numlines + " Pings");
             }
-        } catch (Exception e) {
-            e.fillInStackTrace();
         }
 
-        String contents = new String(bytes);
-        dataText.setText(contents);
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        fileListView.setAdapter(adapter);
+
+        /*
+        Click listener for file list
+         */
+        fileListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                if (!isUploading) {
+                    final String item = (String) parent.getItemAtPosition(position);
+                    view.setBackgroundColor(Color.parseColor("#55EE55"));
+                    //list.remove(item);
+                    //adapter.notifyDataSetChanged();
+
+                    isUploading = true;
+                    // Begin the upload
+                    endpoint_string = urlEditText.getText().toString();
+                    api_string = apiEditText.getText().toString();
+                    // Files were added in reverse order (newest first)
+                    File file = files[files.length - position - 1];
+
+                    UploadTask runner = new UploadTask();
+                    runner.execute(file);
+                }
+            }
+
+        });
+
     }
+
+    /*
+    For counting number of pings in a data file
+    http://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
+     */
+    public static int countLines(File file) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(file));
+        byte[] c = new byte[1024];
+        int count = 0;
+        int readChars = 0;
+        boolean empty = true;
+        try {
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            is.close();
+        }
+        return (count == 0 && !empty) ? 1 : count;
+    }
+
+    /*
+    Uploader
+     */
+    private class UploadTask extends AsyncTask<File, Integer, String> {
+
+        private String resp;
+
+        @Override
+        protected String doInBackground(File... params) {
+            publishProgress(0);
+
+            File file = params[0];
+            String url_string = "http://" + endpoint_string + "/" + api_string + "/ping";
+
+            int total = 1;
+            try{
+                total = countLines(file);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            int attempted = 0;
+            int good = 0;
+
+            // Read each line into json and post it
+            try {
+                URL url = new URL(url_string);
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+
+                    // Construct JSON data object
+                    JSONObject jsonobj;
+                    jsonobj = new JSONObject();
+                    try {
+                        String[] parts = line.split(", ");
+                        jsonobj.put("timestamp", Long.parseLong(parts[0]));
+                        jsonobj.put("accuracy", Float.parseFloat(parts[1]));
+                        jsonobj.put("latitude", Double.parseDouble(parts[2]));
+                        jsonobj.put("longitude", Double.parseDouble(parts[3]));
+                    } catch (JSONException ex) {
+                        resp = "Error Occurred while building JSON";
+                        ex.printStackTrace();
+                    }
+                    // Connect to the api
+                    try {
+                        // Needs to be done every request
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setReadTimeout(10000);
+                        conn.setConnectTimeout(15000);
+                        conn.setRequestMethod("POST");
+                        conn.setDoInput(true);
+                        conn.setDoOutput(true);
+                        conn.setRequestProperty("Content-Type", "application/json");
+                        conn.connect();
+                        OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(),"UTF-8");
+                        // Write json
+                        out.write(jsonobj.toString());
+                        out.flush();
+                        out.close();
+
+                        // Get response
+                        int HttpResult = conn.getResponseCode();
+
+                        StringBuilder sb = new StringBuilder();
+                        if(HttpResult == HttpURLConnection.HTTP_OK){
+                            BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    conn.getInputStream(),"utf-8"));
+                            String inresp = null;
+                            while ((inresp = in.readLine()) != null) {
+                                sb.append(line + "\n");
+                            }
+                            in.close();
+                            resp = sb.toString();
+                            if(resp == "Success") {
+                                good++;
+                            }
+                        } else {
+                            resp =  Integer.toString(HttpResult);
+                        }
+                        attempted++;
+
+                        // Disconnect
+                        conn.disconnect();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        resp = e.getMessage();
+                    }
+                    publishProgress((int) attempted/total);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return resp;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            isUploading = false;
+            statusTextView.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            statusTextView.setText("Preparing for upload...");
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... prog) {
+            progressBarBar.setProgress(prog[0]);
+        }
+    }
+
     /*
     Task runner for network threads
      */
